42d7d2bcbc278e8149576d2a663eff325594bf8c
Mateusz Werpachowski <matyszxzx@gmail.com>
2019-06-05 13:21:16 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let todos = ["wash", "code"];
+  let todo = "";
+
+  function addTodo() {
+    todos = [...todos, todo];
+    todo = "";
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Todo list</h1>
+<input bind:value={todo} placeholder="Enter new todo" />
+<button on:click={addTodo}>Add todo</button>
+<ul>
+  {#each todos as todo}
+    <li>{todo}</li>
+  {/each}
+</ul>
