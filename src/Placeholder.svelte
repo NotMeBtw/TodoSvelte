7f3ebd2d5c660394ca5883bc1e885855caa11ff9
Mateusz Werpachowski <matyszxzx@gmail.com>
2019-06-10 18:00:38 +0200

    changed placeholder's paddings

--- src/Placeholder.svelte
+++ src/Placeholder.svelte
@@ -2,9 +2,21 @@
   import { fly, fade } from "svelte/transition";
 </script>
 
-<p class="has-text-centered is-size-4" in:fly out:fade>
-  <span class="icon">
-    <i class="fas fa-list-ul" />
-  </span>
-  No todos
-</p>
+<style>
+  li {
+    padding: 0 1rem;
+    margin: 1rem 0;
+  }
+  p {
+    padding: 1rem;
+  }
+</style>
+
+<li transition:fade={{ duration: 500 }}>
+  <p class="has-text-centered is-size-4" in:fly out:fade>
+    <span class="icon">
+      <i class="fas fa-list-ul" />
+    </span>
+    No todos
+  </p>
+</li>
